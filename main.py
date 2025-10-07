e10e3d22dac90e60b94980645653fc0b80fa65d0
dariel2222 <203575644+dariel2222@users.noreply.github.com>
2025-10-07 08:22:30 -0400

    actualizar repositorio

---
+++ main.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+import files.config as config
+from apps.calculator import calculator as calc
+
+class App(tk.Tk):
+    pass
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
